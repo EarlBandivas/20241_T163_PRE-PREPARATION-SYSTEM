9b321085f1e10e5fbfedf3a1f8dc9946f96abe46
EarlBandivas <2201103934@student.buksu.edu.ph>
2024-11-07 12:41:50 +0800

    Minor changes and Mainpage added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import LandingPage from './LandingPage';
+import MainPage from './MainPage';
+import AdminPage from './AdminPage';
 
 function App() {
   return (
-    <>
-      <LandingPage />
-    </>
+    <Router>
+      <Routes>
+        {/* <Route path='/' element={<LandingPage />} />
+        <Route path='/Main' element={<MainPage />} /> */}
+        <Route path='/' element={<AdminPage />} />
+      </Routes>
+    </Router>
   );
 }
 
