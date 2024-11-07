975610c44e3df0f0474993de1bb1b1e15fd252f8
EarlBandivas <2201103934@student.buksu.edu.ph>
2024-11-07 12:48:50 +0800

    AdminPage Added

---
+++ Frontend/src/AdminPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './ADComponents/Header';
+import Sidebar from './ADComponents/Sidebar';
+
+function AdminPage() {
+  return (
+    <>
+      <Header />
+      <Sidebar />
+    </>
+  );
+}
+
+export default AdminPage;
