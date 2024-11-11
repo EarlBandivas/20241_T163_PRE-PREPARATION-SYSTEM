045987be635fadab46df74daf746d2f62d52c6c8
EarlBandivas <2201103934@student.buksu.edu.ph>
2024-11-12 01:24:39 +0800

    Logout button added

--- Frontend/src/ADComponents/Header.jsx
+++ Frontend/src/ADComponents/Header.jsx
@@ -3,37 +3,93 @@ import {
   Navbar,
   Typography,
   IconButton,
+  Menu,
+  MenuHandler,
+  MenuList,
+  MenuItem,
   Button,
+  Dialog,
+  DialogHeader,
+  DialogBody,
+  DialogFooter,
   Input,
 } from '@material-tailwind/react';
-import { BellIcon, Cog6ToothIcon } from '@heroicons/react/24/solid';
+import { BellIcon, ChevronDownIcon } from '@heroicons/react/24/solid';
 
 function Header() {
+  const [size, setSize] = React.useState(null);
+
+  const handleOpen = (value) => setSize(value);
   return (
-    <Navbar
-      variant='gradient'
-      color='blue'
-      className='mx-auto max-w-screen-3xl mt-1 px-4 py-3 '
-    >
-      <div className='flex flex-wrap items-center justify-between gap-y-4 text-white'>
-        <Typography
-          as='a'
-          href='#'
-          variant='h6'
-          className='mr-4 ml-2 cursor-pointer py-1.5'
-        >
-          Admin
-        </Typography>
-        <div className='ml-auto flex gap-1 md:mr-4'>
-          <IconButton variant='text' color='white'>
-            <Cog6ToothIcon className='h-4 w-4' />
-          </IconButton>
-          <IconButton variant='text' color='white'>
-            <BellIcon className='h-4 w-4' />
-          </IconButton>
+    <>
+      <Navbar
+        variant='gradient'
+        color='blue'
+        className='mx-auto max-w-screen-3xl mt-1 px-4 py-3 '
+      >
+        <div className='flex flex-wrap items-center justify-between gap-y-4 text-white'>
+          <Typography
+            as='a'
+            href='#'
+            variant='h6'
+            className='mr-4 ml-2 cursor-pointer py-1.5'
+          >
+            Admin
+          </Typography>
+          <div className='ml-auto flex gap-1 md:mr-4'>
+            <IconButton variant='text' color='white'>
+              <BellIcon className='h-4 w-4' />
+            </IconButton>
+            <Menu>
+              <MenuHandler>
+                <IconButton variant='text' color='white'>
+                  <ChevronDownIcon class='h-4 w-4 ' />
+                </IconButton>
+              </MenuHandler>
+              <MenuList>
+                <MenuItem onClick={() => handleOpen('xs')} variant='gradient'>
+                  Create Account
+                </MenuItem>
+                <MenuItem>Logout</MenuItem>
+              </MenuList>
+            </Menu>
+          </div>
         </div>
-      </div>
-    </Navbar>
+      </Navbar>
+      <Dialog
+        open={size === 'xs'}
+        size={size || 'xs'}
+        handler={handleOpen}
+        className='grid grid-cols-1 place-items-center '
+      >
+        <DialogHeader>Create an account.</DialogHeader>
+        <DialogBody>
+          <div className='w-72 m-4'>
+            <Input label='Email Address' />
+          </div>
+          <div className='w-72 m-4'>
+            <Input label='Password' />
+          </div>
+        </DialogBody>
+        <DialogFooter>
+          <Button
+            variant='text'
+            color='red'
+            onClick={() => handleOpen(null)}
+            className='mr-1'
+          >
+            <span>Cancel</span>
+          </Button>
+          <Button
+            variant='gradient'
+            color='green'
+            onClick={() => handleOpen(null)}
+          >
+            <span>Confirm</span>
+          </Button>
+        </DialogFooter>
+      </Dialog>
+    </>
   );
 }
 
